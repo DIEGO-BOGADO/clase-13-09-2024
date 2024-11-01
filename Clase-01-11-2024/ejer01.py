101761f453d5262f365e6bd8f4c11227738ab060
Diego <matibaez234@gmail.com>
2024-11-01 19:25:46 -0300

    nuevo

--- Clase-01-11-2024/ejer01.py
+++ Clase-01-11-2024/ejer01.py
@@ -1,16 +1,10 @@
-""" Ejercicio 01
-Escribir un programa que pida dos números y me de la suma, la resta, la multiplicación, la división y modulo
-"""
-var1 = float(input("Escriba un nro: "))
-var2 = float(input("Escriba un nro: "))
+import random
 
-suma = var1 + var2
-print(f"la suma es : {suma}")
-resta = var1 - var2
-print(f"la resta es : {resta}")
-multiplicacion = var1 * var2
-print(f"la multiplicacion es : {multiplicacion}")
-division = var1 / var2 if var2 != 0 else 0
-print(f"la division es : {division:.2f}")
-modulo = var1 % var2 if var2 != 0 else 0
-print(f"el modulo es : {modulo}")
+def randint():
+    return random.randint(1,100)
+
+print(random.__name__)
+print(__name__)
+for i in range(3):
+    a = randint()
+    print(a)
